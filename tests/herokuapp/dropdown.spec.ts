9899b9600e061ebda55f113acd480e878731fd6a
Lucas Pimenta <lucashfp20@gmail.com>
2025-09-24 18:22:34 -0300

    dropdown

--- tests/herokuapp/dropdown.spec.ts
+++ tests/herokuapp/dropdown.spec.ts
@@ -8,11 +8,11 @@ test('Validar seleção no Dropdown', async ({ page }) => {
   // Click on Dropdown
   await page.click('text=Dropdown');
 
-  //Option 1
-  await page.selectOption('#dropdown', '1'); //Selecionar 1
+  // Option 1
+  await page.selectOption('#dropdown', '1'); // Select 1
   await expect(page.locator('#dropdown')).toHaveValue('1');
 
-  //Option 2
-  await page.selectOption('#dropdown', '2'); //Selecionar 2
+  // Option 2
+  await page.selectOption('#dropdown', '2'); // Select 2
   await expect(page.locator('#dropdown')).toHaveValue('2');
 });
