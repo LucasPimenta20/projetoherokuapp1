f89a1dcc88b658b653059433d36e8bff65170b74
Lucas Pimenta <lucashfp20@gmail.com>
2025-09-24 18:28:55 -0300

    abtesting

--- tests/herokuapp/abtesting.spec.ts
+++ tests/herokuapp/abtesting.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('Validar A/B Testing', async ({ page }) => {
-  //Acessar a página 
+  // Access the page 
   await page.goto('https://the-internet.herokuapp.com/');
 
-  //Clicar em A/B Testing
+  // Click on A/B Testing
   await page.click('text=A/B Testing');
 
-  //Verificar o título
-  const titulo = page.locator('h3'); //Título
+  // Check title
+  const titulo = page.locator('h3'); //Title
   await expect(titulo).toHaveText(/A\/B Test/);
 
-  //Verificar o texto
+  // Check title
   const paragrafo = page.locator('div.example > p');
   await expect(paragrafo).toBeVisible();
 });
